eb7c7b35173c39c91b3b1d375f604f6203925da3
KAVALI PAVAN <145048122+KAVALIPAVAN@users.noreply.github.com>
2025-01-23 20:28:43 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -20,6 +20,9 @@ app.use(
 // middlewares
 app.use(express.json());
 
+app.get('/', function (req, res) {
+  res.send('Hello World')
+});
 // app.use(cookieParser());
 // app.get('/', function (req, res) {
 //   res.send('Hello World');
