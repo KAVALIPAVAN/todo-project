a4ce5dce64a58bb47aca7340168abc5787329019
KAVALI PAVAN <145048122+KAVALIPAVAN@users.noreply.github.com>
2025-01-28 11:57:57 +0530

    Update userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,7 +3,7 @@ import {Post} from "../model/postModel.js";
 import TryCatch from '../middleware/TryCatch.js'
 import getDataUrl from "../controllers/urlGenerator.js";
 
-export const maxDuration=50;
+//export const maxDuration=50;
 
 export const createUser = TryCatch(async (req, res) => {
   const { name,email } = req.body;
