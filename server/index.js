ab86a31fb5dce31766622f90d129c98bb83a7d3b
KavaliPrashanth <kavaliprashanth03@gmail.com>
2025-02-07 14:37:03 +0530

    changes2

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const port = process.env.PORT;
 
 app.use(
   cors({
-    origin: "http://localhost:5173", // Your frontend URL
+    origin: "https://todo-project-frontend-flame.vercel.app", // Your frontend URL
     credentials: true, // ✅ Required for cookies
   })
 );
